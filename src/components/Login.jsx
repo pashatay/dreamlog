54226e93f9579447a5424bb5746b2ad82be5dd62
Pasha Tay <pashabonilla@gmail.com>
2020-01-06 17:19:32 -0500

    .

---
+++ src/components/Login.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import cosmos from "../images/cosmos.mov";
+
+function Login() {
+  return (
+    <>
+      <form className="login-form">
+        <input required type="email" placeholder="email" name={"email"} />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name={"password"}
+        />
+        <input type="submit" className="button" value="Log In"></input>
+      </form>
+      <h5 className="or">or</h5>
+      <div className="div-signup-button">
+        <Link to="signup">
+          <h4 className="signup-button">Sign Up</h4>
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default Login;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,19 +1,32 @@
 import React, { useState, useContext } from "react";
-
+import { Link } from "react-router-dom";
+import Login from "../components/Login";
 import cosmos from "../images/cosmos.mov";
 function Main() {
   return (
-    <div className="div-video-main-page">
-      <video
-        playsinline="playsinline"
-        className="video-main-page"
-        autoplay="autoplay"
-        muted="muted"
-        loop="loop"
-      >
-        <source src={cosmos} />>
-      </video>
-    </div>
+    <>
+      <div className="div-video-main-page">
+        <video
+          playsinline="playsinline"
+          className="video-main-page"
+          autoplay="autoplay"
+          muted="muted"
+          loop="loop"
+        >
+          <source src={cosmos} />>
+        </video>
+        <div className="app-name">
+          <h2>Dream Log - </h2>
+          <p>the best dream journal out there.</p>
+        </div>
+      </div>
+      <div className="div-login-form">
+        <Login />
+      </div>
+      <Link to="/about">
+        <h3 className="about-link">About</h3>
+      </Link>
+    </>
   );
 }
 
