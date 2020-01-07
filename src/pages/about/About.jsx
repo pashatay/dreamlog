a1d3134446fcfcabf36bdabba4891615b15bddfb
Pasha Tay <pashabonilla@gmail.com>
2020-01-06 20:44:32 -0500

    .

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import styles from "./styles.css";
+import Header from "../../components/header/Header";
 
 function About() {
   return (
     <>
+      <Header />
       <h1>About page</h1>
     </>
   );
