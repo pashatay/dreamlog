5f01f8fd74d7c597291b1de56e99e26279e61ee0
Pasha Tay <pashabonilla@gmail.com>
2020-01-06 21:14:15 -0500

    .

--- src/pages/userpage/Userpage.jsx
+++ src/pages/userpage/Userpage.jsx
@@ -8,6 +8,24 @@ function Userpage() {
   return (
     <>
       <Header />
+      <nav>
+        <div>
+          <i class="ri-quill-pen-fill"></i>
+          <h5>Add a dream</h5>
+        </div>
+        <div>
+          <i class="ri-moon-clear-fill"></i>
+          <h5>My dream log</h5>
+        </div>
+        <div>
+          <i class="ri-book-open-fill"></i>
+          <h5>My dream blog</h5>
+        </div>
+        <div>
+          <i class="ri-bar-chart-fill"></i>
+          <h5>My Statistics</h5>
+        </div>
+      </nav>
     </>
   );
 }
