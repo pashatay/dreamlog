d6ba3446089877f09b424992b4df880c51face52
Pasha Tay <pashabonilla@gmail.com>
2020-01-19 14:28:34 -0500

    .

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -8,18 +8,20 @@ function About() {
       <Header />
       <h1>About page</h1>
       <p>
-        All of us have dreams. Dreams can be a powerful tool to understand
-        oneself and are an infinite source of inspiration. The Dream log app is
-        here to help you to keep track of your dreams. We encourage you to write
-        your dreams every morning as soon as you wake up. Give your dream a
-        memorable title and write a short story about that adventure. You can
-        categorize your dream as Lucid, Normal, or Nightmare.   Some dreams are
-        worth sharing. Add your dreams to your dream blog by making them public
-        and share the link with your friends and family. Your dream statistics
-        on the app will show the rate at which you have one type of dream over
-        the others, how many hours of sleep you get, and what your average
-        sleeping hours per dream type are. We keep your personal dream
-        statistics ready, right on your page any time you want to get a better
+        //all devices layout All of us have dreams. Dreams can be a powerful
+        tool to understand oneself and are an infinite source of
+        inspiration. The Dream log app is here to help you to keep track of your
+        dreams. //dream log We encourage you to write your dreams every morning
+        as soon as you wake up. Give your dream a memorable title and write a
+        short story about that adventure. You can categorize your dream as
+        Lucid, Normal, or Nightmare.   //add dream Some dreams are worth
+        sharing. Add your dreams to your dream blog by making them public and
+        share the link with your friends and family. You can change the privacy
+        of any dream, anytime. //dream blog Your dream statistics on the app
+        will show the rate at which you have one type of dream over the others,
+        how many hours of sleep you get, and what your average sleeping hours
+        per dream type are. //staistics We keep your personal dream statistics
+        ready, right on your page any time you want to get a better
         understanding of your dream data. Join our Dream log family today and
         start your journey into understanding your subconscious psyche!
       </p>
--- src/pages/dreamlog/Dreamlog.jsx
+++ src/pages/dreamlog/Dreamlog.jsx
@@ -19,6 +19,7 @@ function Dreamlog() {
   } = useContext(Context);
 
   useEffect(() => {
+    setDreamFilter("all");
     getAllDreams();
   }, []);
 
